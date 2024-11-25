8c0fcaa9f5dec3020decf1d0893963c3764ecac5
junx21 <jxuche@myuax.com>
2024-11-25 17:11:54 +0100

    main

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "EJ1/EJ1.h"
+#include "EJ2/EJ2.h"
+#include "EJ3/EJ3.h"
+
+int main() {
+    printf("Ejercicio 1a:\n");
+    ejercicio1a();
+
+    printf("\nEjercicio 1b:\n");
+    ejercicio1b();
+
+    printf("\nEjercicio 2a:\n");
+    ejercicio2a();
+
+    printf("\nEjercicio 2b:\n");
+    ejercicio2b();
+
+    printf("\nEjercicio 3a:\n");
+    ejercicio3a();
+
+    printf("\nEjercicio 3b:\n");
+    ejercicio3b();
+
+    return 0;
+}
